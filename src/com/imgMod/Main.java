877b49a1672766bba69b55fb439eb701b3694120
swagenseller <jwagense@ucsc.edu>
2020-03-26 15:20:46 -0700

    added main method to read in an image file

--- src/com/imgMod/Main.java
+++ src/com/imgMod/Main.java
@@ -1,8 +1,40 @@
 package com.imgMod;
+import java.util.*;
+
+//import java.awt.Color;
+//import java.awt.Image;
+//import java.awt.Graphics2D;
+import java.awt.image.BufferedImage; // importing buffered Image class
+//import java.awt.image.DataBufferDouble;
+import java.io.File; // importing File class
+import java.io.IOException;  // imports IOException
+//import java.net.URL;
+
+//import javax.imageio.IIOImage;
+import javax.imageio.ImageIO; // importing imageIO module
+//import javax.imageio.ImageWriteParam;
+//import javax.imageio.ImageWriter;
+//import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+//import javax.imageio.stream.ImageOutputStream;
+//import javax.imageio.*;
+
 
 public class Main {
+    static BufferedImage pic;
+    public static void main(String[] args) throws Exception, IOException {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Welcome, enter the filename of your image:");
+        String picName = scan.next();
+        BufferedImage imageName = readImage(picName);
+    }
 
-    public static void main(String[] args) {
-	// write your code here
+    ////////////////////////////////////////////////////////////////
+    //  The following reads in an image with a matching file name //
+    ////////////////////////////////////////////////////////////////
+
+    public static BufferedImage readImage(String fname) throws Exception{
+
+        BufferedImage image = ImageIO.read(new File(fname) );
+        return(image);
     }
 }
